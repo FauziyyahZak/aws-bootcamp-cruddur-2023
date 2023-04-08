8b849158fbb968b1f1aee79876ee6735927b2ca0
Fauziyah Zakariyah <fauziyyahzak@gmail.com>
2023-04-08 17:28:10 +0000

    Update user details

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,7 +2,7 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Fauziyah Zakariyah', 'fauziyyahzak@gmail.com', 'fauzak' ,'MOCK'),
-  ('Zakariyah Fauziyah', 'fauziyyahzak@gmail.com', 'fauz' ,'MOCK'),
+  ('Zakariyah Fauziyah', 'bintumubaaraka@gmail.com', 'fauz' ,'MOCK'),
   ('Zakariyah Bilqiis', 'biliki@gmail.com', 'chukwundi' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
